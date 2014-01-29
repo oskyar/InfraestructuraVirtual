098b149b5b8e01f76a15a536456489203e6f511e
Oscar Zafra <oskyar@gmail.com>
2014-01-29 12:27:43 +0100

    configuracion rapida chef añadida

---
+++ Tema6/default.rb
@@ -0,0 +1,12 @@
+
+
+package 'emacs'
+directory '/home/oskyar/proyectosGit/InfraestructuraVirtual/Tema6
+'
+file "/home/oskyar/Documentos/LEEME" do
+	owner "jmerelo"
+	group "jmerelo"
+	mode 00544
+	action :create
+	content "Directorio para documentos diversos"
+end
---
+++ Tema6/solo.rb
@@ -0,0 +1,3 @@
+file_cache_path "/home/oskyar/chef"
+cookbook_path "/home/oskyar/chef/cookbooks"
+json_attribs "/home/oskyar/chef/node.json"
